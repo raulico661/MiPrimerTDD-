0dcc1b40913358c8614d84cb46d829f109e5f66d
Raúl <raulelche2001@gmail.com>
2023-03-24 19:37:25 +0100

    Refactorizado todos los métodos y test con mi nombre

--- Test/TestCoche.java
+++ Test/TestCoche.java
@@ -3,31 +3,31 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero_RaulSempere(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta_RaulSempere(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerarRaulSempere(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye_RaulSempere(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerarRaulSempere(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_RaulSempere(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerarRaulSempere(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
